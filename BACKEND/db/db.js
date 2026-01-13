af25ec30343ee2270124847d0998e5c0e0067d8f
Nilesh Jadhav <nileshjadhav93078@gmail.com>
2026-01-13 20:56:34 +0530

    Initial commit

--- BACKEND/db/db.js
+++ BACKEND/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+//dummy change
 function connectToDb() {
   mongoose
     .connect(process.env.DB_CONNECT)              //DB_CONECT is from .env file A DATABASE URL 
